67a9793a9ca3afb4e5583fe195f759f9bfe4f8eb
AbanobNageh <abanob.nageh159@gmail.com>
2024-10-18 17:46:09 +0300

    Updated end to end tests to add negative cases.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,21 +21,55 @@ describe('Action Tracker (e2e)', () => {
   });
 
   describe('Users endpoints', () => {
-    it('/:userId (GET)', () => {
-      return request(app.getHttpServer()).get('/users/1').expect(200).expect({
-        id: 1,
-        name: 'Ferdinande',
-        createdAt: '2020-07-14T05:48:54.798Z',
+    describe('/:userId (GET)', () => {
+      it('/:userId (GET) - invalid user id.', () => {
+        return request(app.getHttpServer())
+          .get('/users/aa')
+          .expect(400)
+          .expect({
+            statusCode: 400,
+            message: 'Some of the request data is invalid.',
+            validationErrors: [
+              'userId must not be less than 0',
+              'userId must be a number conforming to the specified constraints',
+            ],
+            error: 'Bad Request',
+          });
+      });
+
+      it('/:userId (GET)', () => {
+        return request(app.getHttpServer()).get('/users/1').expect(200).expect({
+          id: 1,
+          name: 'Ferdinande',
+          createdAt: '2020-07-14T05:48:54.798Z',
+        });
       });
     });
 
-    it('/:userId/action-count (GET)', () => {
-      return request(app.getHttpServer())
-        .get('/users/1/action-count')
-        .expect(200)
-        .expect({
-          count: 49,
-        });
+    describe('/:userId/action-count (GET)', () => {
+      it('/:userId/action-count (GET) - invalid user id.', () => {
+        return request(app.getHttpServer())
+          .get('/users/a/action-count')
+          .expect(400)
+          .expect({
+            statusCode: 400,
+            message: 'Some of the request data is invalid.',
+            validationErrors: [
+              'userId must not be less than 0',
+              'userId must be a number conforming to the specified constraints',
+            ],
+            error: 'Bad Request',
+          });
+      });
+
+      it('/:userId/action-count (GET)', () => {
+        return request(app.getHttpServer())
+          .get('/users/1/action-count')
+          .expect(200)
+          .expect({
+            count: 49,
+          });
+      });
     });
 
     it('/referral-index (GET)', () => {
@@ -51,18 +85,32 @@ describe('Action Tracker (e2e)', () => {
   });
 
   describe('Actions endpoints', () => {
-    it('/:actionType/next-actions (GET)', () => {
-      return request(app.getHttpServer())
-        .get('/actions/WELCOME/next-actions')
-        .expect(200)
-        .expect({
-          WELCOME: 0.16805845511482254,
-          CONNECT_CRM: 0.06680584551148225,
-          EDIT_CONTACT: 0.2630480167014614,
-          ADD_CONTACT: 0.23382045929018788,
-          VIEW_CONTACTS: 0.25260960334029225,
-          REFER_USER: 0.015657620041753653,
-        });
+    describe('/:actionType/next-actions (GET)', () => {
+      it('/:actionType/next-actions (GET) - invalid action type.', () => {
+        return request(app.getHttpServer())
+          .get('/actions/a/next-actions')
+          .expect(400)
+          .expect({
+            statusCode: 400,
+            message: 'Some of the request data is invalid.',
+            validationErrors: ['actionType must be a valid action type.'],
+            error: 'Bad Request',
+          });
+      });
+
+      it('/:actionType/next-actions (GET)', () => {
+        return request(app.getHttpServer())
+          .get('/actions/WELCOME/next-actions')
+          .expect(200)
+          .expect({
+            WELCOME: 0.16805845511482254,
+            CONNECT_CRM: 0.06680584551148225,
+            EDIT_CONTACT: 0.2630480167014614,
+            ADD_CONTACT: 0.23382045929018788,
+            VIEW_CONTACTS: 0.25260960334029225,
+            REFER_USER: 0.015657620041753653,
+          });
+      });
     });
   });
 });
